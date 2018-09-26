f60f3e4e1683fd22a56433a5d6e0b3012a684906
Rogerio de Oliveira <rogerio.oliveira@venturus.org.br>
2018-09-26 16:12:59 -0300

    WIP: add test to verify the sentiment analysis

--- app/src/androidTest/java/sample/study/happytwitter/instrumented/tweetslist/TweetsListInstrumented.kt
+++ app/src/androidTest/java/sample/study/happytwitter/instrumented/tweetslist/TweetsListInstrumented.kt
@@ -1,17 +1,25 @@
 package sample.study.happytwitter.instrumented.tweetslist
 
 import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions
 import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.assertion.ViewAssertions
 import android.support.test.espresso.contrib.RecyclerViewActions
 import android.support.test.espresso.matcher.ViewMatchers
+import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.filters.LargeTest
 import android.support.test.runner.AndroidJUnit4
 import android.support.v7.widget.RecyclerView
+import org.hamcrest.core.AllOf
 import org.junit.Test
 import org.junit.runner.RunWith
 import sample.study.happytwitter.R
 import sample.study.happytwitter.instrumented.GenericTestClass
 import sample.study.happytwitter.utils.CommonTestFunctions
+import sample.study.happytwitter.utils.EspressoTestsMatchers
+import sample.study.happytwitter.utils.RecyclerViewMatcher
+import java.util.EnumSet.allOf
+import java.util.regex.Pattern.matches
 
 
 @LargeTest
@@ -29,5 +37,26 @@ class TweetsListInstrumented:GenericTestClass() {
         onView(ViewMatchers.withId(R.id.tweets_recyclerview))
                 .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(POSITION, click()))
         CommonTestFunctions.assertElementIsDisplayed("JOYFUL to meet you at Venturus!")
+    }
+
+    @Test
+    fun testVerifyImageIsDisplayed(){
+        CommonTestFunctions.typeText(R.id.username_edittext, validTwitterName)
+        onView(ViewMatchers.withId(R.id.tweets_recyclerview))
+                .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(POSITION, click()))
+        onView(AllOf.allOf(withId(R.id.tweet_sentiment_imageview),
+                isDescendantOfA(RecyclerViewMatcher(R.id.tweets_recyclerview)
+                .atPosition(POSITION))))
+                .check(ViewAssertions.matches(isDisplayed()))
+    }
+
+    @Test
+    fun testVerifyHappyImage(){
+        CommonTestFunctions.typeText(R.id.username_edittext, validTwitterName)
+        onView(ViewMatchers.withId(R.id.tweets_recyclerview))
+                .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(POSITION, click()))
+        onView(AllOf.allOf(CommonTestFunctions.drawableIsCorrect(R.drawable.tweetlist_item_happy_bg),
+                isDescendantOfA(RecyclerViewMatcher(R.id.tweets_recyclerview).atPosition(POSITION))))
+                .check(ViewAssertions.matches(isDisplayed()))
     }
 }
--- app/src/androidTest/java/sample/study/happytwitter/utils/CommonTestFunctions.kt
+++ app/src/androidTest/java/sample/study/happytwitter/utils/CommonTestFunctions.kt
@@ -1,5 +1,8 @@
 package sample.study.happytwitter.utils
 
+import android.content.Context
+import android.graphics.drawable.BitmapDrawable
+import android.support.annotation.DrawableRes
 import android.support.test.espresso.Espresso
 import android.support.test.espresso.Espresso.*
 import android.support.test.espresso.NoMatchingViewException
@@ -9,11 +12,15 @@ import android.support.test.espresso.action.ViewActions
 import android.support.test.espresso.assertion.ViewAssertions
 import android.support.test.espresso.matcher.ViewMatchers
 import android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom
+import android.support.v4.content.ContextCompat
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
+import org.hamcrest.Description
 import org.hamcrest.Matchers.not
 import org.hamcrest.core.AllOf
 import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -207,6 +214,30 @@ class CommonTestFunctions internal constructor() {
         fun getFileName(testName: String): String {
             val date = SimpleDateFormat("yyyyMMddHHmmss").format(Date())
             return testName + "_" + date
+        }
+
+        fun drawableIsCorrect(@DrawableRes drawableResId: Int): Matcher<View> {
+            return object : TypeSafeMatcher<View>() {
+                override fun describeTo(description: Description) {
+                    description.appendText("with drawable from resource id: ")
+                    description.appendValue(drawableResId)
+                }
+
+                override fun matchesSafely(target: View?): Boolean {
+                    if (target !is ImageView) {
+                        return false
+                    }
+                    if (drawableResId < 0) {
+                        return target.drawable == null
+                    }
+                    val expectedDrawable = ContextCompat.getDrawable(target.context, drawableResId)
+                            ?: return false
+
+                    val bitmap = (target.drawable as BitmapDrawable).bitmap
+                    val otherBitmap = (expectedDrawable as BitmapDrawable).bitmap
+                    return bitmap.sameAs(otherBitmap)
+                }
+            }
         }
 
     }
