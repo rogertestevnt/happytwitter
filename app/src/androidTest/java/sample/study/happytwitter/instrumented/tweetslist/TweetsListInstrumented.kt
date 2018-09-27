e5ca71d6fc4563d1b83ebe20bf8fb0a99cc219c2
Rogerio de Oliveira <rogerio.oliveira@venturus.org.br>
2018-09-27 08:33:51 -0300

    Add a image view test

--- app/src/androidTest/java/sample/study/happytwitter/instrumented/tweetslist/TweetsListInstrumented.kt
+++ app/src/androidTest/java/sample/study/happytwitter/instrumented/tweetslist/TweetsListInstrumented.kt
@@ -2,16 +2,20 @@ package sample.study.happytwitter.instrumented.tweetslist
 
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.assertion.ViewAssertions
 import android.support.test.espresso.contrib.RecyclerViewActions
 import android.support.test.espresso.matcher.ViewMatchers
+import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.filters.LargeTest
 import android.support.test.runner.AndroidJUnit4
 import android.support.v7.widget.RecyclerView
+import org.hamcrest.core.AllOf
 import org.junit.Test
 import org.junit.runner.RunWith
 import sample.study.happytwitter.R
 import sample.study.happytwitter.instrumented.GenericTestClass
 import sample.study.happytwitter.utils.CommonTestFunctions
+import sample.study.happytwitter.utils.RecyclerViewMatcher
 
 
 @LargeTest
@@ -29,5 +33,15 @@ class TweetsListInstrumented:GenericTestClass() {
         onView(ViewMatchers.withId(R.id.tweets_recyclerview))
                 .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(POSITION, click()))
         CommonTestFunctions.assertElementIsDisplayed("JOYFUL to meet you at Venturus!")
+    }
+
+    @Test
+    fun testVerifyImageViewIsDisplayed(){
+        CommonTestFunctions.typeText(R.id.username_edittext, validTwitterName)
+        onView(ViewMatchers.withId(R.id.tweets_recyclerview))
+                .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(POSITION, click()))
+        onView(AllOf.allOf(withId(R.id.tweet_sentiment_imageview),
+                isDescendantOfA(RecyclerViewMatcher(R.id.tweets_recyclerview).atPosition(POSITION))))
+                .check(ViewAssertions.matches(isDisplayed()))
     }
 }
